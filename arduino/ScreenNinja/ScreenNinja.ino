1553601ab4cc1d0a7432f5d636c864d52d321d3a
Brian Antonelli <brian.antonelli@gmail.com>
2012-05-12 00:14:50 -0400

    Bluetooth Working
    Got BT working on the Arduino and on the Android app. Sending some
    small sample data to the phone which is successfully receiving and
    printing it out! w00t!

--- arduino/ScreenNinja/ScreenNinja.ino
+++ arduino/ScreenNinja/ScreenNinja.ino
@@ -1,15 +1,18 @@
 #include <MeetAndroid.h>
 
+// Bluetooth MAC: 00:06:66:08:E7:60
+MeetAndroid meetAndroid;
+
 const int rightSignalPin = 8;  // 0v off; ~10v on
 const int leftSignalPin  = 9;  // 0v off; ~10v on
 const int highBeamPin    = 10; // 0v off; 12v on
-const int neutralPin     = 11; // 10v in gear; 0v in neutral
-const int lowFuelPin     = 12; // ~4.5-5v low fuel; ?v full
+const int neutralPin     = 11; // 10v in gear; 0v in neutral (NEGATIVE TRIGGER)
+const int lowFuelPin     = 12; // ~4.5-5v low fuel; ?v full (NEGATIVE TRIGGER)
 const int tachSignalPin  = 13; // 0v off; ~6.5v running
 
 void setup() {
-  Serial.begin(9600); // USB
-//  Serial.begin(57600); // BLUETOOTH
+//  Serial.begin(9600); // USB
+  Serial.begin(115200); // BLUETOOTH
   pinMode(highBeamPin,    INPUT);
   pinMode(leftSignalPin,  INPUT);
   pinMode(rightSignalPin, INPUT);
@@ -19,22 +22,27 @@ void setup() {
 }
 
 void loop() {
-//  meetAndroid.receive();
-//  meetAndroid.send(1);
+  meetAndroid.receive();
+
+  String out = "_";
   int val = analogRead(rightSignalPin);
-  if(val > 100){
-    Serial.println("RIGHT BLINKER IS ON!");
-  }
+  out += (val > 100) ? "1" : "0";
+  out += "|";
 
   val = analogRead(leftSignalPin);
-  if(val > 100){
-    Serial.println("LEFT BLINKER IS ON!");
-  }
+  out += (val > 100) ? "1" : "0";
+  out += "|";
   
   val = analogRead(highBeamPin);
-  if(val > 100){
-    Serial.println("HIGH BEAMS ARE ON!");
-  }  
-
+  out += (val > 100) ? "1" : "0";
+  
+  // Prepare string to send
+  out += "~";
+  int length = out.length();
+  char charBuff[length+1];
+  out.toCharArray(charBuff, length+1);
+  
+  meetAndroid.send(charBuff);
+  
   delay(500);
 }
--- src/com/ninjascreen/NinjaScreenActivity.java
+++ src/com/ninjascreen/NinjaScreenActivity.java
@@ -13,8 +13,8 @@ import at.abraxas.amarino.AmarinoIntent;
 public class NinjaScreenActivity extends Activity {
 	private static final String TAG = "NinjaScreen";
 	
-	// TODO: Change to my BT's MAC and ensure device is added to Anduino
-	private static final String DEVICE_ADDRESS = "00:00:00:00:00:00";
+	// BT's MAC on Arduino
+	private static final String DEVICE_ADDRESS = "00:06:66:08:E7:60";
 	
 	private ArduinoReceiver arduino = new ArduinoReceiver(); 
 	
@@ -29,10 +29,15 @@ public class NinjaScreenActivity extends Activity {
     	super.onStart();
     	
     	// Register our receiver so we can receive broadcasts
+    	Log.d(TAG, "Registering receiver");
     	registerReceiver(arduino, new IntentFilter(AmarinoIntent.ACTION_RECEIVED));
+    	registerReceiver(arduino, new IntentFilter(AmarinoIntent.ACTION_CONNECTED));
     	
     	// Connect to the device
+    	Log.d(TAG, "Connecting to Arduino");
     	Amarino.connect(this, DEVICE_ADDRESS);
+    	
+    	//Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'l', 0);
     }
     
     @Override
@@ -40,9 +45,12 @@ public class NinjaScreenActivity extends Activity {
     	super.onStop();
     	
     	// Always disconnect the Arduino when exiting the app
+    	Log.d(TAG, "Disconnecting from Arduino");
     	Amarino.disconnect(this, DEVICE_ADDRESS);
     	
+    	
     	// And un-register
+    	Log.d(TAG, "Unregistering receiver");
     	unregisterReceiver(arduino);
     }
     
@@ -50,8 +58,12 @@ public class NinjaScreenActivity extends Activity {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
+			Log.d(TAG, "*-rx-*");
 	    	String data = null;
 	    	final String address = intent.getStringExtra(AmarinoIntent.EXTRA_DEVICE_ADDRESS);
+	    	
+	    	Log.d(TAG, "Received Event From: " + address);
+	    	
 	    	final int dataType = intent.getIntExtra(AmarinoIntent.EXTRA_DATA_TYPE, -1);
 			
 	    	if(dataType == AmarinoIntent.STRING_EXTRA){
@@ -59,6 +71,13 @@ public class NinjaScreenActivity extends Activity {
 	    		
 	    		if(data != null){
 	    			Log.d(NinjaScreenActivity.TAG, "Data is = " + data);
+	    			if(data.charAt(0) != '_' || data.charAt(data.length()-1) != '~'){
+	    				Log.e(TAG, "First or last special char missing! Trash data!");
+	    			}
+	    			else{
+	    				String[] peices = data.substring(1, data.length()-1).split("\\|");
+	    				Log.d(TAG, "Right signal: " + peices[0].equals("1") + ", Left signal: " + peices[1].equals("1") + ", High beams: " + peices[2].equals("1"));
+	    			}
 	    		}
 	    	}
 		}
